174e901c6ec74f345b59816b304eaaefbbebbdca
YAMAL-YENG <nickykipruto@yahoo.com>
2025-05-31 04:54:01 +0300

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const response = await axios.get(url);
         const translation = response.data.responseData.translatedText;
 
-        const translationMessage = `> *CASEYRHODES-XMD-TRANSLATION*
+        const translationMessage = `> *SIGMA-MD-TRANSLATION*
 
 > 🔤 *Original*: ${textToTranslate}
 
