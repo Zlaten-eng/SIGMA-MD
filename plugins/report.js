f18324e070ef459061ae5a44c764941412a3a47e
YAMAL-YENG <nickykipruto@yahoo.com>
2025-05-31 04:17:18 +0300

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -22,7 +22,7 @@ cmd({
         }
 
         const reportedMessages = {};
-        const devNumber = "254112192119"; // Bot owner's number
+        const devNumber = "254769677305"; // Bot owner's number
         const messageId = m.key.id;
 
         if (reportedMessages[messageId]) {
